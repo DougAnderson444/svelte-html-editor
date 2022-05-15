383675e2236e78dd44492714308985ae8ac46cf8
Doug A <douganderson444@gmail.com>
2022-05-15 14:50:13 -0300

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Editor from '$lib/Editor.svelte';
+	let html = '<h3>Hello</h3>';
+	let editor;
+</script>
+
+<Editor {html} on:change={(evt) => (html = evt.detail)} bind:this={editor} />
+
+<style>
+	.cl-content {
+		/* resize: both; */
+	}
+</style>
